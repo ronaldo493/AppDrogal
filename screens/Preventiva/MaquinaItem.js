e64ba5901cc918573413ec654a632232bfe421cb
ronaldo493 <94649532+ronaldo493@users.noreply.github.com>
2024-11-15 01:00:50 -0300

    Tela Preventiva - Camera

--- screens/Preventiva/MaquinaItem.js
+++ screens/Preventiva/MaquinaItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { View, Button, Text, TextInput, Modal, Alert } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import PatrimonioAssinaturaStyles from "../styles/PatrimonioAssinaturaStyles";
@@ -13,30 +13,74 @@ export default MaquinaItem = ({ item }) => {
 
   const { label, options = [], requiresSelection } = item;
 
-  //Estado para armazenar o número do patrimônio escaneado
-  
-
   //Estado para o valor selecionado no Picker
   const [selectedValue, setSelectedValue] = useState(options[0]?.value);
 
   //Estado para permissão, abrir o scanner, armazenar o patrimonio
-  const {permission, requestPermission} = useCameraPermissions();
+  const [permission, requestPermission] = useCameraPermissions();
   const [isScannerVisible, setScannerVisible] = useState(false);
+
+  //Estado para armazenar o número do patrimônio escaneado
   const [patrimonio, setPatrimonio] = useState("");
+  
+  //Estado para controle do flash
+  const [flashEnabled, setFlashEnabled] = useState('off')
+  
+  //Estado para bloquear a câmera em caso de erro
+  const [isCameraActive, setIsCameraActive] = useState(true); // Câmera ativa por padrão
+
+  //Evitar múltiplas leituras
+  const codigoLock = useRef(false)
 
   //Função para scanear o patrimonio
   const handleScan = async () => {
-    const { granted } = await requestPermission()
-    if(!granted){
-      return Alert.alert("Câmera, Você precisa habilitar o uso da camêra!")
+    const { granted } = await requestPermission();
+    if (!granted) {
+      return Alert.alert("Câmera", "Você precisa habilitar o uso da câmera!");
     }
+    codigoLock.current = false;
     setScannerVisible(true);
   };
 
-  //Função Patrimõnio Lido
-  const handleScanRead=() =>{
+  //Função Patrimônio Lido
+  const handleScanRead = ({ data }) => {
+    if (codigoLock.current) return; //Ignorar se já foi lido
 
-  }
+    //Se o código for maior que 6 dígitos, não realiza a leitura
+    if (data.length > 6) {
+      Alert.alert("Código inválido", "O código de barras deve ter no máximo 6 dígitos.", [
+        {
+          text: "OK",
+          onPress: () => {
+            setIsCameraActive(true); //Reativar a câmera após o alerta
+            setScannerVisible(true); //Continuar escaneando
+          },
+        },
+      ]);
+      setIsCameraActive(false); //Bloquear a câmera até o usuário clicar em "OK"
+      return;
+    }
+
+    //Se o código já foi lido, exibe um alerta
+    if (patrimonio && patrimonio === data) {
+      Alert.alert("Código já lido", "Este código de barras já foi escaneado.", [
+        {
+          text: "OK",
+          onPress: () => {
+            setIsCameraActive(true); //Reativar a câmera após o alerta
+            setScannerVisible(true); //Continuar escaneando
+          },
+        },
+      ]);
+      setIsCameraActive(false); //Bloquear a câmera até o usuário clicar em "OK"
+      return;
+    }
+
+    codigoLock.current = true; //Bloquear para leituras futuras
+    setPatrimonio(data); //Atualizar o valor de patrimônio
+    setScannerVisible(false); //Desativa a visualização da câmera após o código válido ser lido
+    console.log("Patrimônio escaneado:", data);
+  };
 
   return (
     <View>
@@ -46,7 +90,7 @@ export default MaquinaItem = ({ item }) => {
         <TextInput
           style={[PatrimonioAssinaturaStyles.PatrimonioInput, themeStyles.inputPatrimonio]}
           value={patrimonio}
-          onChangeText={setPatrimonio}
+          onChangeText={(text) => setPatrimonio(text.replace(/\D/g, '').slice(0, 6))} //Restringir a 6 dígitos
           placeholder="PATRIMÔNIO"
           placeholderTextColor={isDarkMode ? '#ccc' : '#666'}
           keyboardType="numeric" 
@@ -63,7 +107,7 @@ export default MaquinaItem = ({ item }) => {
         <Picker
           selectedValue={selectedValue}
           onValueChange={(itemValue) => setSelectedValue(itemValue)}
-          style={{ color: isDarkMode ? '#ccc' : '#000', }}
+          style={{ color: isDarkMode ? '#ccc' : '#000' }}
         >
           {options.map((option, index) => (
             <Picker.Item key={index} label={option.label} value={option.value} />
@@ -72,19 +116,22 @@ export default MaquinaItem = ({ item }) => {
       ) : null}
 
       <Modal visible={isScannerVisible}>
-        <CameraView facing="back" style={PatrimonioAssinaturaStyles.modalScan}>
-          <View style={PatrimonioAssinaturaStyles.viewButton}>
-            <Button 
-              title="Cancelar"
-              style={PatrimonioAssinaturaStyles.modalButton} 
-              color={isDarkMode ? '#777' : '#BB5059'} 
-              onPress={() => setScannerVisible(false)}>
-            </Button>
-          </View>
-
-        </CameraView>
+        {isCameraActive && (
+          <CameraView 
+            facing="back" 
+            style={PatrimonioAssinaturaStyles.modalScan}
+            onBarcodeScanned={handleScanRead}
+          >
+            <View style={PatrimonioAssinaturaStyles.viewButton}>
+              <Button 
+                title="Cancelar"
+                style={PatrimonioAssinaturaStyles.modalButton} 
+                color={isDarkMode ? '#777' : '#BB5059'} 
+                onPress={() => setScannerVisible(false)} />
+            </View>
+          </CameraView>
+        )}
       </Modal>
     </View>
   );
 };
-
